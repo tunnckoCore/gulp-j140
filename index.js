90f462447898fe54bc7705bb875c9a8f5aa2b953
tunnckoCore <mameto_100@mail.bg>
2014-12-30 10:40:21 +0200

    move lib/index.js to root

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 /**
- * gulp-j140 - index.js
- * Copyright (c) 2014
- * MIT Licensed
+ * gulp-j140 <https://github.com/tunnckoCore/gulp-j140>
  *
- * @author  Charlike Mike Reagent (@tunnckoCore)
- * @api private
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
  */
-module.exports = require('./lib/');
+
+'use strict';
+
+var gutil = require('gulp-util');
+var through = require('through2');
+var j140 = require('j140');
+
+module.exports = function gulpJ140(data) {
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-j140', 'Streaming not supported'));
+      return cb();
+    }
+    try {
+      file.contents = new Buffer(j140(file.contents.toString(), data));
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-j140', err));
+    }
+    this.push(file);
+    cb();
+  });
+};
